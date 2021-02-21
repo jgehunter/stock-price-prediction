e24aa952a292b488789e5b5d17869e450e1dce20
jgehunter <jgehunter@gmail.com>
2021-02-21 11:51:42 +0100

    adjusted setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='stock_price_prediction',
       version='0.0',
       description='Library to predict stock prices',
       author='Jorge Hunter ÁLvarez',
-      packages=['stock_price_prediction', 'stock_price_prediction.download'],
+      packages=find_packages(),
      )
