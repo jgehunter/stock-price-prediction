601a9a9dddf8bfa7eda3ebd1366205986e7e8a75
jgehunter <jgehunter@gmail.com>
2021-02-21 11:40:34 +0100

    added package setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='stock_price_prediction',
+      version='0.0',
+      description='Library to predict stock prices',
+      author='Jorge Hunter ÁLvarez',
+      packages=['stock_price_prediction', 'stock_price_prediction.download'],
+     )
